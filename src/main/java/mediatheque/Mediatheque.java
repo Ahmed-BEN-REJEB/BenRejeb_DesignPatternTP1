6de420093f6ab2a99133fc616c99182dd89bd08d
Ahmed-BEN-REJEB <ahmed.ben_rejeb@etud.univ-jfc.fr>
2025-09-12 17:23:15 +0200

    Modification de la classe "Mediatheque.java"

--- src/main/java/mediatheque/Mediatheque.java
+++ src/main/java/mediatheque/Mediatheque.java
@@ -4,29 +4,16 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Mediatheque {
-	private final List<Item> items = new LinkedList<>();
+	private final List<Item> items = new LinkedList<>(); 
 
 	public void addItem(Item i) {
 		items.add(i);
 	}
 	
-	public void printCatalog() {
-		for (Item i : items)
-			i.print();
-	}
-	
-	public void printOnlyBooks() {
-		throw new UnsupportedOperationException("Not supported yet."); 
-		/*
-		//avec instanceof
-		for (Item i : items)
-			if (i instanceof Book)
-				System.out.println(i);
-		*/
-	}
-
-	public void printOnlyCDs() {
-		throw new UnsupportedOperationException("Not supported yet."); 
-	}
+	public void applyVisitor(Visitor v) {
+        for (Item i : items) {
+            i.accept(v);
+        }
+    }
 
 }
